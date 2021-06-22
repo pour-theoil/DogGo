88e6ec53ecbbc6f6e9dc94d7f5a3842376c99c4c
Logan Demmy <frank.demmy@gmail.com>
2021-06-22 14:26:18 -0500

    added initail coding

---
+++ DogGo/Repositories/IDogRepository.cs
@@ -0,0 +1,18 @@
+﻿using DogGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Repositories
+{
+    public interface IDogRepository
+    {
+        List<Dog> GetAll();
+        Dog GetDogById(int id);
+        void AddDog(Dog dog);
+        void DeleteDog(int dogId);
+        void UpdateDog(Dog dog);
+
+    }
+}
